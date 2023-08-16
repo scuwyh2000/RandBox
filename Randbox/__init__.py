a6647a1897fca3eeea0b05aaa677e91480863295
scuwyh2000 <87008857+scuwyh2000@users.noreply.github.com>
2023-08-16 11:10:33 +0800

    Delete Randbox directory

--- Randbox/__init__.py
+++
@@ -1,5 +0,0 @@
-from .config import add_RandBox_config
-from .detector import RandBox
-from .dataset_mapper import RandBoxDatasetMapper
-from .test_time_augmentation import RandBoxWithTTA
-from .swintransformer import build_swintransformer_fpn_backbone
