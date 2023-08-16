a86be58f031f2567df82deaaf75cbb9f4b7ae86f
scuwyh2000 <87008857+scuwyh2000@users.noreply.github.com>
2023-08-16 13:14:28 +0800

    Update train_net.py

--- train_net.py
+++ train_net.py
@@ -24,14 +24,14 @@ from detectron2.evaluation import COCOEvaluator, LVISEvaluator, verify_results
 from detectron2.solver.build import maybe_add_gradient_clipping
 from detectron2.modeling import build_model
 
-from randBox import RandBoxDatasetMapper, add_RandBox_config, RandBoxWithTTA
-from randBox.util.model_ema import add_model_ema_configs, may_build_model_ema, may_get_ema_checkpointer, EMAHook, \
+from randbox import RandBoxDatasetMapper, add_RandBox_config, RandBoxWithTTA
+from randbox.util.model_ema import add_model_ema_configs, may_build_model_ema, may_get_ema_checkpointer, EMAHook, \
     apply_model_ema_and_restore, EMADetectionCheckpointer
 
 
 from detectron2.data import DatasetCatalog, MetadataCatalog
 from detectron2.data.datasets.coco import load_coco_json
-from randBox.pascal_voc_evaluation import PascalVOCDetectionEvaluator
+from randbox.pascal_voc_evaluation import PascalVOCDetectionEvaluator
 
 class Register:
     """用于注册自己的数据集"""
