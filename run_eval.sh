141d68a9e504c370b5dc08dcd2940eb40c4aa884
scuwyh2000 <87008857+scuwyh2000@users.noreply.github.com>
2023-09-26 00:42:47 +0800

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python train_net.py --num-gpus 2 --task t1 --config-file configs/t1.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
+python train_net.py --num-gpus 1 --task t1 --config-file configs/t1.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
 
-python train_net.py --num-gpus 2 --task t2_ft --config-file configs/t2_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
+python train_net.py --num-gpus 1 --task t2_ft --config-file configs/t2_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
 
-python train_net.py --num-gpus 2 --task t3_ft --config-file configs/t3_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
+python train_net.py --num-gpus 1 --task t3_ft --config-file configs/t3_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
 
-python train_net.py --num-gpus 2 --task t4_ft --config-file configs/t4_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
+python train_net.py --num-gpus 1 --task t4_ft --config-file configs/t4_ft.yaml --eval-only MODEL.WEIGHTS path/to/model.pth
